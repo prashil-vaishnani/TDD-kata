738b5b37cb173dc9718623a493b9694f0e4ba457
Prashil Vaishnani <prashil.v@simformsolutions.com>
2025-06-21 10:53:42 +0530

    feat: update Add function to pass empty string test case

--- src/utils/stringCalculator.ts
+++ src/utils/stringCalculator.ts
@@ -1,4 +1,7 @@
 // src/utils/stringCalculator.ts
-
-export function Add(numbers: string) {
+export function Add(numbers: string): number {
+  if (numbers === "") {
+    return 0;
+  }
+  return 0;
 }
